8105bb0e37b1f9b5156ad19a4c655edaeb9c07fa
summer <cythia.xy@gmail.com>
2018-12-22 13:39:05 +0800

    分享图

--- pages/activity-detail/activity-detail.js
+++ pages/activity-detail/activity-detail.js
@@ -8,13 +8,20 @@ Page({
      */
     data: {
         id: '',
-        signUpNo: '',
         showForm: false,
-        status: null, // 0: 未参与; 1: 参与进行中; 2: 完成积赞; 3: 帮好友点赞; 
-        num: 10,
-        finishedCount: null,
-        performableCount: null,
-        precentNum: null,
+        signUpNo: '',
+        myProgress: {
+            status: null, // 0: 未参与; 1: 参与进行中; 2: 完成积赞;
+            finishedCount: null,
+            performableCount: null,
+            precentNum: null,
+        },
+        otherProgress: {
+            status: null, // 帮好友点赞; 
+            finishedCount: null,
+            performableCount: null,
+            precentNum: null,
+        },
         shareBgImg: '',
         cxCodeImg: ''
     },
@@ -23,76 +30,71 @@ Page({
         if (options.signUpNo) {
             this.data.signUpNo = options.signUpNo
         }
-        let _this = this
-        wx.getImageInfo({
-            src: 'https://light-real.oss-cn-hangzhou.aliyuncs.com/6011812210000002.png',
-            success: function (res) {
-                _this.data.shareBgImg = res.path
-            }
-        })
-        wx.getImageInfo({
-            src: 'https://light-real.oss-cn-hangzhou.aliyuncs.com/6011812190000030.png',
-            success: function (res) {
-                _this.data.cxCodeImg = res.path
-            }
-        })
     },
     onShow: function () {
+        wx.showLoading()
+        if (this.data.signUpNo) {
+            this.getOthersProgress()
+        }
         this.getUserProgress()
     },
     // 查看进度
     getUserProgress() {
-        if (this.data.signUpNo) {
-            this.getOthersProgress()
-        } else {
-            // 查看自己的进度
-            Api.getUserProgress(this.data.id).then(res => {
-                wx.hideNavigationBarLoading() //在标题栏中隐藏加载
-                wx.stopPullDownRefresh() //停止下拉刷新
-                switch (res.data.status) {
-                    // 未参与
-                    case 'NOT_INVOLVED':
-                        {
-                            this.setData({
-                                status: 0,
-                            })
-                            break;
-                        }
-                        // 进行中
-                    case 'ONGOING':
-                        {
-                            this.setData({
-                                status: 1,
-                                finishedCount: res.data.finishedCount,
-                                performableCount: res.data.performableCount,
-                                precentNum: parseInt(res.data.finishedCount / res.data.performableCount * 100)
-                            })
-                            break;
-                        }
-                        // 已完成
-                    case 'FINISHED':
-                        {
-                            this.setData({
-                                status: 2,
-                            })
-                            break;
-                        }
-                }
-            })
-        }
+        // 查看自己的进度
+        Api.getUserProgress(this.data.id).then(res => {
+            wx.hideNavigationBarLoading()
+            wx.stopPullDownRefresh()
+            wx.hideLoading()
+            switch (res.data.status) {
+                // 未参与
+                case 'NOT_INVOLVED':
+                    {
+                        this.setData({
+                            'myProgress.status': 0,
+                        })
+                        break;
+                    }
+                    // 进行中
+                case 'ONGOING':
+                    {
+                        this.setData({
+                            'myProgress.status': 1,
+                            'myProgress.finishedCount': res.data.finishedCount,
+                            'myProgress.performableCount': res.data.performableCount,
+                            'myProgress.precentNum': parseInt(res.data.finishedCount / res.data.performableCount * 100)
+                        })
+                        break;
+                    }
+                    // 已完成
+                case 'FINISHED':
+                    {
+                        this.setData({
+                            'myProgress.status': 2,
+                        })
+                        break;
+                    }
+            }
+        })
     },
     // 查看朋友的进度
     getOthersProgress() {
         Api.getOthersProgress(this.data.signUpNo).then(res => {
-            wx.hideNavigationBarLoading() //在标题栏中隐藏加载
-            wx.stopPullDownRefresh() //停止下拉刷新
+            wx.hideNavigationBarLoading()
+            wx.stopPullDownRefresh()
+            wx.hideLoading()
             this.setData({
-                status: 3,
-                finishedCount: res.data.finishedCount,
-                performableCount: res.data.performableCount,
-                precentNum: parseInt(res.data.finishedCount / res.data.performableCount * 100)
+                'otherProgress.status': 3,
+                'otherProgress.finishedCount': res.data.finishedCount,
+                'otherProgress.performableCount': res.data.performableCount,
+                'otherProgress.precentNum': parseInt(res.data.finishedCount / res.data.performableCount * 100)
             })
         })
+    },
+    // 从好友分享页面切换到我的进度页面
+    showMyProgress() {
+        wx.redirectTo({
+            url: `/pages/activity-detail/activity-detail?id=${this.data.id}`
+        });
     },
     // 帮好友点赞
     helpFriend() {
@@ -166,7 +168,7 @@ Page({
         Api.applyActivity(postData).then(res => {
             if (res.success) {
                 this.setData({
-                    status: 1,
+                    'myProgress.status': 1,
                     showForm: false
                 })
                 wx.showToast({
@@ -198,38 +200,60 @@ Page({
         if (this.data.signUpNo) {
             param = `activityNo=${this.data.id}&signUpNo=${this.data.signUpNo}`
         } else {
-            param = `activityNo=${this.data.id}`
+            return
         }
+        console.log(param)
         return {
             imageUrl: 'https://light-real.oss-cn-hangzhou.aliyuncs.com/6011812210000001.png',
             title: '帮我助力，免费领取平安果，你也可以参加哦',
             path: `pages/activity-detail/activity-detail?${param}`
         }
     },
-    // 生成分享图到本地相册
-    createImg() {
+    // 预先生成分享图
+    getShareBgImg() {
         wx.showLoading({
             title: '正在生成分享图...',
             mask: true
         })
-        const context = wx.createCanvasContext('shareCanvas')
-        context.drawImage(this.data.shareBgImg, 0, 0, 375, 667)
-        context.drawImage(this.data.cxCodeImg, 0, 0, 100, 100, 140, 330, 100, 100)
-        context.draw(true, setTimeout(function () {
-            wx.canvasToTempFilePath({
-                canvasId: 'shareCanvas',
+        let _this = this
+        return new Promise((resolve) => {
+            wx.getImageInfo({
+                src: 'https://light-real.oss-cn-hangzhou.aliyuncs.com/6011812210000002.png',
                 success: function (res) {
-                    wx.saveImageToPhotosAlbum({
-                        filePath: res.tempFilePath,
-                    })
-                    wx.hideLoading()
-                    wx.showToast({
-                        title: '保存本地成功，可以在朋友圈分享哦～',
-                        icon: 'none',
-                        duration: 3000
+                    _this.data.shareBgImg = res.path
+                    wx.getImageInfo({
+                        src: 'https://light-real.oss-cn-hangzhou.aliyuncs.com/6011812190000030.png',
+                        success: function (res) {
+                            _this.data.cxCodeImg = res.path
+                            resolve()
+                        }
                     })
                 }
             })
-        }, 2000))
+        })
+    },
+    // 生成分享图到本地相册
+    createImg() {
+        this.getShareBgImg().then(() => {
+            const context = wx.createCanvasContext('shareCanvas')
+            context.drawImage(this.data.shareBgImg, 0, 0, 375, 667)
+            context.drawImage(this.data.cxCodeImg, 0, 0, 100, 100, 140, 330, 100, 100)
+            context.draw(true, function () {
+                wx.canvasToTempFilePath({
+                    canvasId: 'shareCanvas',
+                    success: function (res) {
+                        wx.saveImageToPhotosAlbum({
+                            filePath: res.tempFilePath,
+                        })
+                        wx.hideLoading()
+                        wx.showToast({
+                            title: '保存本地成功，可以在朋友圈分享哦～',
+                            icon: 'none',
+                            duration: 3000
+                        })
+                    }
+                })
+            })
+        })
     }
 })
